f2206777040fbda19783bb407f9b73c5ac4a210b
Moss <danielsmoss96@gmail.com>
2022-05-08 14:53:02 +0100

    Added functionality for the nav bar to become sticky on scroll

---
+++ app.js
@@ -0,0 +1,14 @@
+let navbar = document.getElementById("navbar")
+let sticky = navbar.offsetTop;
+
+
+window.onscroll = function() {checkSticky()};
+
+function checkSticky() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
