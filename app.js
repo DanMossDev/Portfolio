78b29c1f23659672e070f835648c0176cf802ada
Moss <danielsmoss96@gmail.com>
2022-05-14 20:59:09 +0100

    Attempted to add load on scroll for some elements - bug testing in progress

--- app.js
+++ app.js
@@ -14,3 +14,19 @@ function checkSticky() {
         navbar.classList.remove("sticky");
     }
 }
+
+
+//Handles items fading in on scroll
+let loadArray = document.getElementsByClassName("notloaded");
+
+window.onscroll = function() {checkLoad()};
+
+function checkLoad() {
+    for (let load of loadArray) {
+      if (document.documentElement.scrollTop > load.getBoundingClientRect().top) {
+          load.classList.remove("notloaded");
+          load.classList.add("loaded");
+          loadArray = document.getElementsByClassName("notloaded");
+      }
+    }
+}
