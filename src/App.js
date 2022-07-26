acf5cd7ca9ca215d947186a7144bc3f9defa1053
Moss <danielsmoss96@gmail.com>
2022-07-26 23:41:15 +0100

    add new project links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import tindie from './images/tindie.jpg'
 import worldle from './images/worldle.jpg'
+import spotyou from './images/spotyou.jpg'
 import { useState } from 'react';
 import Navbar from './components/Navbar'
 import Landing from './components/Landing';
@@ -11,19 +12,19 @@ import ProjectDetails from './components/ProjectDetails';
 function App() {
   const [currentProject, setCurrentProject] = useState()
 
-  return ( <>
+  return <>
     <Navbar />
     <div className="main">
     <Landing />
     <About />
     <section id="projects" className="projects">
       <Project setCurrentProject={setCurrentProject} title="Tindie" liveURL="https://danmossdev.github.io/Tindie/" gitURL="https://github.com/DanMossDev/Tindie" imgURL={tindie} bio="Match with your perfect indie game"/>
+      <Project setCurrentProject={setCurrentProject} title="Spot|You" liveURL="https://danmossdev.github.io/ill-spot-you-bro/" gitURL="https://github.com/DanMossDev/ill-spot-you-bro/" imgURL={spotyou} bio="Transfer Spotify playlists to YouTube"/>
       <Project setCurrentProject={setCurrentProject} title="Worldle" liveURL="https://danmossdev.github.io/Worldle/" gitURL="https://github.com/DanMossDev/Worldle" imgURL={worldle} bio="A geography game to boggle the mind"/>
     </section>
     <ProjectDetails currentProject={currentProject} setCurrentProject={setCurrentProject}/>
     </div>
   </>
-  );
 }
 
 export default App;
